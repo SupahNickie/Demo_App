47af15ea0cbdecf1f578806c0cbf1f5fe4aa2fa4
Nicholas Case <nicholascase@live.com>
2013-08-04 08:40:42 -0700

    Final/Only Commit (Small App)
    Ready to run locally on Rails development server.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c7bb9293aa0afd64e89da2c366402b2dbc76862bfedc5123b81740555a2121a0a972c06e20cb429787070bd6b4036cbfc94e8ddaa131337bd28d15ef55ea988f'
